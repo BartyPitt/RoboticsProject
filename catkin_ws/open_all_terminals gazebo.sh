58e1b9a42d76a7184170465964eef54b214ca6af
MedadRufus <medadrufus@gmail.com>
2020-02-28 17:49:14 +0000

    Update open_all_terminals gazebo.sh

--- catkin_ws/open_all_terminals gazebo.sh
+++ catkin_ws/open_all_terminals gazebo.sh
@@ -1,6 +1,8 @@
 gnome-terminal --tab --title="$title1" --command="bash -c 'roscore; $SHELL'" \
-               --tab --title="$title3" --command="bash -c 'sleep 5s;source devel/setup.bash;rosrun gazebo_ros gazebo; $SHELL'"\
-               --tab --title="$title2" --command="bash -c 'sleep 15s;source devel/setup.bash;roslaunch franka_gazebo panda_arm_hand.launch; $SHELL'" \
-               --tab --title="$title3" --command="bash -c 'sleep 15s;source devel/setup.bash;roslaunch panda_moveit_config demo.launch rviz_tutorial:=true; $SHELL'"\
-               --tab --title="$title3" --command="bash -c 'sleep 15s;cd src/panda_publisher;python panda_publisher.py; $SHELL'"
+               --tab --title="$title3" --command="bash -c 'sleep 5s; source devel/setup.bash; rosrun gazebo_ros gazebo; $SHELL'"\
+               --tab --title="$title3" --command="bash -c 'sleep 5s; source devel/setup.bash; roslaunch panda_moveit_config demo.launch rviz_tutorial:=true; $SHELL'"\
+               --tab --title="$title3" --command="bash -c 'sleep 5s; cd src/panda_publisher; python panda_publisher.py; $SHELL'"\
+               --tab --title="$title2" --command="bash -c 'sleep 10s; source devel/setup.bash; roslaunch franka_gazebo panda_arm_hand.launch; $SHELL'" 
+
+
 
